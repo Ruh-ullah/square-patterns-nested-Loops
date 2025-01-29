f48895589acecc626812204e49d3676e9a8df2ce
Ruh-ullah <ruh500500@gmail.com>
2025-01-29 09:46:50 +0500

    Add files via upload

---
+++ Sum by for loop.cpp
@@ -0,0 +1,10 @@
+//enter number to sum.
+int n;
+int sum=0;
+cout << "Enter Number to sum :";
+cin >> n; 
+
+for (int i = 1;i <=n;i++) {
+	sum = sum+i;
+}
+cout << " sum "<< sum << endl;
---
+++ multiplication by for loop.cpp
@@ -0,0 +1,6 @@
+int n; 
+cout << "Enter number yout it's table :";
+cin >> n;
+for (int i = 1;i <= 10;i++) {
+	cout << n << "x" << i << "=" << n * i << endl;
+}
